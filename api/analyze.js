36ad9041efa890f4a1009951d3b99f13f02bdce9
tybn6jr5j2-spec <tybn6jr5j2@privaterelay.appleid.com>
2026-05-25 23:26:56 +0200

    Add files via upload

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  reexports.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(200).end();
